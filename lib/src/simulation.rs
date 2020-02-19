941065cb657b75dff797f9cf5bfc3fd7d297d013
Justin Shrake <jshrake@users.noreply.github.com>
2020-02-19 08:10:48 -0800

    Remove the tapland check (#3)
    I simply don't have confidence that this code is correct and do not wish
    to maintain it going forward.

--- lib/src/simulation.rs
+++ lib/src/simulation.rs
@@ -26,7 +26,6 @@ pub struct Observations {
   pub mana: usize,
   pub cmc: usize,
   pub play: usize,
-  pub tapped: usize,
   pub in_opening_hand: usize,
   pub total_runs: usize,
 }
@@ -45,14 +44,6 @@ impl Observations {
 
   pub fn p_play(&self) -> f64 {
     self.play as f64 / self.total_runs as f64
-  }
-
-  pub fn p_tapped(&self) -> f64 {
-    self.tapped as f64 / self.total_runs as f64
-  }
-
-  pub fn p_tapped_given_cmc(&self) -> f64 {
-    self.tapped as f64 / self.cmc as f64
   }
 }
 
@@ -120,12 +111,9 @@ impl Simulation {
         if result.in_opening_hand || result.in_draw_hand {
           observations.play += 1;
         }
-      } else if result.tapped {
-        // We can't pay AND we saw a maybe_tapped event? Count a tapped on curve event
-        observations.tapped += 1;
       }
     }
-    assert!(observations.tapped + observations.mana <= observations.cmc);
+    assert!(observations.mana <= observations.cmc);
     observations
   }
 }
@@ -196,7 +184,6 @@ mod tests {
     assert_eq!(obs.cmc, runs);
     assert_eq!(obs.mana, runs);
     assert_eq!(obs.play, runs);
-    assert_eq!(obs.tapped, 0);
   }
 
   // on the draw, always draw all 8 cards
@@ -223,7 +210,6 @@ mod tests {
     assert_eq!(obs.cmc, runs);
     assert_eq!(obs.mana, runs);
     assert_eq!(obs.play, runs);
-    assert_eq!(obs.tapped, 0);
   }
 
   #[test]
@@ -249,7 +235,6 @@ mod tests {
     assert_eq!(obs.cmc, runs);
     assert_eq!(obs.mana, runs);
     assert_eq!(obs.play, runs);
-    assert_eq!(obs.tapped, 0);
     assert_eq!(obs.in_opening_hand, runs);
   }
 
@@ -277,7 +262,6 @@ mod tests {
     assert_eq!(obs.cmc, runs);
     assert_eq!(obs.mana, runs);
     assert_eq!(obs.play, runs);
-    assert_eq!(obs.tapped, 0);
     assert_eq!(obs.in_opening_hand, runs);
   }
 
@@ -340,144 +324,7 @@ mod tests {
       on_the_play: true,
     });
     let o = sim.observations_for_card(ALL_CARDS.card_from_name("Integrity").unwrap());
-    assert!(o.mana + o.tapped == o.cmc);
-  }
-
-  // http://mtgoncurve.com/?v0=eyJjb2RlIjoiNCBUZW1wbGUgb2YgTXlzdGVyeVxuMyBHaWxkZWQgR29vc2UgKEVMRCkgMTYwXG4xIEZvcmVzdCIsIm9uX3RoZV9wbGF5Ijp0cnVlLCJpbml0aWFsX2hhbmRfc2l6ZSI6NywibXVsbGlnYW5fZG93bl90byI6NywibXVsbGlnYW5fb25fbGFuZHMiOlswLDEsNiw3XSwiY2FyZHNfdG9fa2VlcCI6IiJ9
-  #[test]
-  // The idea, there are 8 cards and we pick 7 at random (8 choose 7 = 8)
-  // One of these selections does not contain the forest, which would cause
-  // us to not be able to tap because our hand is full of tap lands
-  fn contrived_tap_test_0() {
-    let code = "
-        3 Gilded Goose
-        4 Temple of Mystery
-        1 Forest
-    ";
-    let deck = ALL_CARDS.from_deck_list(code).expect("Bad deckcode").0;
-    let draws = 1;
-    let runs = 10000;
-    let sim = Simulation::from_config(&SimulationConfig {
-      run_count: runs,
-      draw_count: draws,
-      mulligan: &Never::never(),
-      deck: &deck,
-      on_the_play: true,
-    });
-    // Go first -- no draws so need to play out of hand
-    let obs = sim.observations_for_card(&deck.cards[0]);
-    // denominator is (8 choose 7 = 8)
-    assert!(f64::abs(obs.p_tapped() - 1.0 / 8.0) < 0.01);
-    assert!(f64::abs(obs.p_mana() - 7.0 / 8.0) < 0.01);
-    assert!(f64::abs(obs.p_mana_given_cmc() - 7.0 / 8.0) < 0.01);
-    // Go second -- if you dont have the forest, then you get to draw
-    // and play the forest, never tapped
-    let sim = Simulation::from_config(&SimulationConfig {
-      run_count: runs,
-      draw_count: draws,
-      mulligan: &Never::never(),
-      deck: &deck,
-      on_the_play: false,
-    });
-    let obs = sim.observations_for_card(&deck.cards[0]);
-    assert_eq!(obs.tapped, 0);
-  }
-
-  #[test]
-  fn contrived_tap_test_1() {
-    let code = "
-            1 Yarok, the Desecrated
-            1 Temple of Deceit
-            1 Temple of Mystery
-            1 Watery Grave
-            1 Overgrown Tomb
-            3 Fabled Passage
-        ";
-    let deck = ALL_CARDS.from_deck_list(code).expect("Bad deckcode").0;
-    let draws = 4;
-    let runs = 1000;
-    let sim = Simulation::from_config(&SimulationConfig {
-      run_count: runs,
-      draw_count: draws,
-      mulligan: &Never::never(),
-      deck: &deck,
-      on_the_play: false,
-    });
-    let obs = sim.observations_for_card(ALL_CARDS.card_from_name("Yarok, the Desecrated").unwrap());
-    // Never tapped because by turn 5 (Yarok CMC = 5), we have drawn all of our deck (on turn 1)
-    // and should have been able to play our lands in an appropriate way to avoid tapping on turn 5
-    assert_eq!(obs.tapped, 0);
-    assert_eq!(obs.cmc, runs);
-    assert_eq!(obs.mana, runs);
-  }
-
-  #[test]
-  fn contrived_tap_test_2() {
-    let code = "
-            3 Wind-Scarred Crag
-            13 Plains
-            6 Mountain
-            38 Venerable Knight
-        ";
-    let deck = ALL_CARDS.from_deck_list(code).expect("Bad deckcode").0;
-    let draws = 0;
-    let runs = 10000;
-    let sim = Simulation::from_config(&SimulationConfig {
-      run_count: runs,
-      draw_count: draws,
-      mulligan: &Never::never(),
-      deck: &deck,
-      on_the_play: true,
-    });
-    let obs = sim.observations_for_card(ALL_CARDS.card_from_name("Venerable Knight").unwrap());
-    // ((3 choose 1)*(13 choose 0)*(44 choose 6) + (3 choose 2) * (13 choose 0) * (44 choose 5) + (3 choose 3) * (13 choose 0) * (44 choose 4)) / (60 choose 7) = 0.06362
-    assert!(f64::abs(obs.p_tapped() - 0.06362) < 0.01);
-  }
-
-  #[test]
-  fn contrived_tap_test_3() {
-    let code = "
-            45 Hydroblast
-            10 Island
-            5 Temple of Deceit
-        ";
-    let deck = ALL_CARDS.from_deck_list(code).expect("Bad deckcode").0;
-    let draws = 0;
-    let runs = 10000;
-    let sim = Simulation::from_config(&SimulationConfig {
-      run_count: runs,
-      draw_count: draws,
-      mulligan: &Never::never(),
-      deck: &deck,
-      on_the_play: true,
-    });
-    let obs = sim.observations_for_card(ALL_CARDS.card_from_name("Hydroblast").unwrap());
-    // ((5 choose 1)*(10 choose 0)*(45 choose 6) + (5 choose 2) * (10 choose 0) * (45 choose 5) + (5 choose 3) * (10 choose 0) * (45 choose 4) + (5 choose 4)*(10 choose 0)*(45 choose 3) + (5 choose 5)*(10 choose 0)*(45 choose 2)) / (60 choose 7) = 0.14112
-    assert!(f64::abs(obs.p_tapped() - 0.14112) < 0.01);
-  }
-
-  #[test]
-  fn tap_test_with_hybrid_mana_0() {
-    let code = "
-            38 Integrity
-            6 Mountain
-            8 Plains
-            3 Wind-Scarred Crag
-            1 Sacred Foundry
-            4 Tournament Grounds
-        ";
-    let deck = ALL_CARDS.from_deck_list(code).expect("Bad deckcode").0;
-    let draws = 0;
-    let runs = 10000;
-    let sim = Simulation::from_config(&SimulationConfig {
-      run_count: runs,
-      draw_count: draws,
-      mulligan: &Never::never(),
-      deck: &deck,
-      on_the_play: true,
-    });
-    let o = sim.observations_for_card(ALL_CARDS.card_from_name("Integrity").unwrap());
-    assert!(o.mana + o.tapped == o.cmc);
+    assert!(o.mana == o.cmc);
   }
 
   #[test]
@@ -563,96 +410,6 @@ mod tests {
     let actual = obs.p_mana();
     // All 17 blue lands are valid
     let expected = 0.917; // Hypergeometric, 60, 17, 7, 1
-    let difference = f64::abs(expected - actual);
-    assert!(difference < 0.01); // To within 1%
-  }
-
-  #[test]
-  fn hypergeometric_2() {
-    let code = "
-        2 Cleansing Nova (M19) 9
-        1 Vraska, Relic Seeker (XLN) 232
-        4 Sinister Sabotage (GRN) 54
-        4 Opt (XLN) 65
-        2 Vraska's Contempt (XLN) 129
-        2 Isolated Chapel (DAR) 241
-        3 Cry of the Carnarium (RNA) 70
-        1 Devious Cover-Up (GRN) 35
-        3 Teferi, Hero of Dominaria (DAR) 207
-        3 Hydroid Krasis (RNA) 183
-        2 Assassin's Trophy (GRN) 152
-        2 Overgrown Tomb (GRN) 253
-        3 Breeding Pool (RNA) 246
-        3 Meandering River
-        2 Moment of Craving (RIX) 79
-        3 Hallowed Fountain (RNA) 251
-        4 Dimir Guildgate
-        4 Godless Shrine (RNA) 248
-        2 Search for Azcanta (XLN) 74
-        3 Chemister's Insight (GRN) 32
-        2 Wilderness Reclamation (RNA) 149
-        1 Mastermind's Acquisition (RIX) 77
-        4 Watery Grave (GRN) 259
-        ";
-    let deck = ALL_CARDS.from_deck_list(code).expect("Bad deckcode").0;
-    let draws = 8;
-    let runs = 20000;
-    let sim = Simulation::from_config(&SimulationConfig {
-      run_count: runs,
-      draw_count: draws,
-      mulligan: &Never::never(),
-      deck: &deck,
-      on_the_play: true,
-    });
-    let obs = sim.observations_for_card(ALL_CARDS.card_from_name("Opt").unwrap());
-    let actual = obs.p_mana();
-    // Only 10 of the 17 blue land sources can enter turn 1 untapped. The other 7 are taplands
-    let expected = 0.741370766; // Hypergeometric, 60, 10, 7, 1
-    let difference = f64::abs(expected - actual);
-    assert!(difference < 0.01); // To within 1%
-  }
-
-  #[test]
-  fn hypergeometric_3() {
-    let code = "
-        2 Cleansing Nova (M19) 9
-        1 Vraska, Relic Seeker (XLN) 232
-        4 Sinister Sabotage (GRN) 54
-        4 Opt (XLN) 65
-        2 Vraska's Contempt (XLN) 129
-        2 Isolated Chapel (DAR) 241
-        3 Cry of the Carnarium (RNA) 70
-        1 Devious Cover-Up (GRN) 35
-        3 Teferi, Hero of Dominaria (DAR) 207
-        3 Hydroid Krasis (RNA) 183
-        2 Assassin's Trophy (GRN) 152
-        2 Overgrown Tomb (GRN) 253
-        3 Breeding Pool (RNA) 246
-        3 Glacial Fortress (XLN) 255
-        2 Moment of Craving (RIX) 79
-        3 Hallowed Fountain (RNA) 251
-        4 Dimir Guildgate
-        4 Godless Shrine (RNA) 248
-        2 Search for Azcanta (XLN) 74
-        3 Chemister's Insight (GRN) 32
-        2 Wilderness Reclamation (RNA) 149
-        1 Mastermind's Acquisition (RIX) 77
-        4 Watery Grave (GRN) 259
-        ";
-    let deck = ALL_CARDS.from_deck_list(code).expect("Bad deckcode").0;
-    let runs = 20000;
-    let draws = 8;
-    let sim = Simulation::from_config(&SimulationConfig {
-      run_count: runs,
-      draw_count: draws,
-      mulligan: &Never::never(),
-      deck: &deck,
-      on_the_play: true,
-    });
-    let obs = sim.observations_for_card(ALL_CARDS.card_from_name("Opt").unwrap());
-    let actual = obs.p_mana();
-    // Only 13 of the 17 blue land sources can enter turn 1 untapped. The other 4 are tap ands
-    let expected = 0.837; // Hypergeometric, 60, 13, 7, 1
     let difference = f64::abs(expected - actual);
     assert!(difference < 0.01); // To within 1%
   }
@@ -866,11 +623,10 @@ mod tests {
     let obs = sim.observations_for_card(card);
     assert_eq!(obs.cmc, runs);
     assert_eq!(obs.mana, runs);
-    assert_eq!(obs.tapped, 0);
   }
 
   #[test]
-  fn syr_no_tapped() {
+  fn syr_no_mana() {
     let code = "
       2 Syr Gwyn, Hero of Ashvale (ELD) 330
       4 Dalakos, Crafter of Wonders (THB) 212
@@ -906,6 +662,5 @@ mod tests {
     let obs = sim.observations_for_card(card);
     dbg!(obs);
     assert_eq!(obs.mana, 0);
-    assert_eq!(obs.tapped, 0);
   }
 }
